96958a5fee4ac5c635fba8908dde3a80c027abdb
suhyeon <102345466+jeong-su-hyeon@users.noreply.github.com>
2026-05-23 19:06:23 +0900

    update: MoimApplication.java 어노테이션 추가
    @SpringBootApplication 어노테이션 추가

--- server/src/main/java/com/moim/MoimApplication.java
+++ server/src/main/java/com/moim/MoimApplication.java
@@ -1,7 +1,9 @@
 package com.moim;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MoimApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoimApplication.class, args);
